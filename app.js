da232560a3430eb4a3298e37070e9181ef757313
sachin5424 <sachinsagar516@gmail.com>
2021-01-29 18:31:21 +0530

    add nodejs packages cors and body-parser

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyparser = require('body-parser')
 const router = express.Router()
 const mongoose = require('mongoose')
+app.use(cors())
+app.use(bodyparser.json())
 app.use('/',router)
 
 mongoose.connect('mongodb://127.0.0.1:27017/auth',{
@@ -15,14 +19,13 @@ const User = require('./model/user')
 
 var auth =async (req,res,next)=>{
    let headers = req.headers;
-   if(headers.email){
+   if(headers.email && headers.password){
     let email = headers.email;
     let password = headers.password
     const data =  await User.find()
     for(var k in data){
-        console.log(data[k]);
+        // console.log(data[k]);
         if(email==data[k].email && password==data[k].password){
-            console.log('ok');
             next()
         }
     } 
@@ -33,14 +36,11 @@ var auth =async (req,res,next)=>{
 
 }
 router.get('/',auth,async(req,res,next)=>{
+     
     try {
      const data = await User.find()
      res.status(200).send(data)
      next()
-     for(var k in data){
-         console.log(data[k].username);
-         console.log(data[k].password);
-     }
     } catch (error) {
         res.status(500).send(error)
     }
